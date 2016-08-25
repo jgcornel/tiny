e520ac35b67d7ec3a9c9e30c8ebe88570346402c
Jan G. Cornelis <jgcornel@gmail.com>
2016-08-25 10:58:30 +0200

    Minor modifications

--- print_int.s
+++ print_int.s
@@ -1,18 +1,18 @@
-	.section .bss
-	.lcomm my_buf, 12
-	.section .text
-	.globl print_int
-	.type print_int @function
+    .section .bss
+    .lcomm my_buf, 12
+    .section .text
+    .globl print_int
+    .type print_int @function
 
 print_int:
-	pushl %ebp
+    pushl %ebp
     pushl %eax
     pushl %ebx
     pushl %ecx
     pushl %edx
     pushl %edi
     pushl %esi
-	movl %esp, %ebp
+    movl %esp, %ebp
     movl $my_buf, %edi
     movl $11, %esi
     movl $10, (%edi, %esi, 4)
@@ -49,20 +49,20 @@ print_int:
     decl %esi
 .PI4:
     incl %esi
-	movl $4, %eax
-	movl $1, %ebx
+    movl $4, %eax
+    movl $1, %ebx
     leal (%edi, %esi, 4), %ecx
-	#movl $my_buf, %ecx
-	movl $48, %edx
-	int $0x80
+    #movl $my_buf, %ecx
+    movl $48, %edx
+    int $0x80
 
-	# postlude
-	movl %ebp, %esp
+    # postlude
+    movl %ebp, %esp
     popl %esi
     popl %edi
     popl %edx
     popl %ecx
     popl %ebx
     popl %eax
-	popl %ebp
-	ret
+    popl %ebp
+    ret
